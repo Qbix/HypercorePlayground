960ccbf766a51c22629ffbc4b638306000d4cc77
Pavan Yogi <pavanyogi121@gmail.com>
2021-12-02 15:45:23 +0530

    Update changes related to rebase api refactor

--- p2p-multiwriter-with-autobase/05-Hypernews/hypernews.js
+++ p2p-multiwriter-with-autobase/05-Hypernews/hypernews.js
@@ -1,117 +1,142 @@
-import minimist from 'minimist'
-import Corestore from 'corestore'
-import Hyperswarm from 'hyperswarm'
-import Autobase from 'autobase'
-import Hyperbee from 'hyperbee'
-import crypto from 'crypto'
-import lexint from 'lexicographic-integer'
-import ram from 'random-access-memory'
+import minimist from 'minimist';
+import Corestore from 'corestore';
+import Hyperswarm from 'hyperswarm';
+import Autobase from 'autobase';
+import Hyperbee from 'hyperbee';
+import crypto from 'crypto';
+import lexint from 'lexicographic-integer';
+import ram from 'random-access-memory';
 
 const args = minimist(process.argv, {
   alias: {
     writers: 'w',
     indexes: 'i',
     storage: 's',
-    name: 'n'
+    name: 'n',
   },
   default: {
-    swarm: true
+    swarm: true,
   },
-  boolean: ['ram', 'swarm']
-})
+  boolean: ['ram', 'swarm'],
+});
 
 class Hypernews {
-  constructor () {
-    this.store = new Corestore(args.ram ? ram : (args.storage || 'hypernews'))
-    this.swarm = null
-    this.autobase = null
-    this.bee = null
-    this.name = null
+  constructor() {
+    this.store = new Corestore(args.ram ? ram : args.storage || 'hypernews');
+    this.swarm = null;
+    this.autobase = null;
+    this.bee = null;
+    this.name = null;
   }
 
-  async start () {
-    const writer = this.store.get({ name: 'writer' })
-    const autobaseIndex = this.store.get({ name: 'index' })
+  async start() {
+    const writer = this.store.get({ name: 'writer' });
+    const autobaseIndex = this.store.get({ name: 'index' });
 
-    await writer.ready()
+    await writer.ready();
 
-    this.name = args.name || writer.key.slice(0, 8).toString('hex')
-    this.autobase = new Autobase([writer], { indexes: autobaseIndex })
+    this.name = args.name || writer.key.slice(0, 8).toString('hex');
+    this.autobase = new Autobase([writer], { indexes: autobaseIndex });
 
     for (const w of [].concat(args.writers || [])) {
-      await this.autobase.addInput(this.store.get(Buffer.from(w, 'hex')))
+      await this.autobase.addInput(this.store.get(Buffer.from(w, 'hex')));
     }
 
     for (const i of [].concat(args.indexes || [])) {
-      await this.autobase.addDefaultIndex(this.store.get(Buffer.from(i, 'hex')))
+      await this.autobase.addDefaultIndex(
+        this.store.get(Buffer.from(i, 'hex'))
+      );
     }
 
-    await this.autobase.ready()
+    await this.autobase.ready();
 
     if (args.swarm) {
-      const topic = Buffer.from(sha256(this.name), 'hex')
-      this.swarm = new Hyperswarm()
-      this.swarm.on('connection', (socket) => this.store.replicate(socket))
-      this.swarm.join(topic)
-      await this.swarm.flush()
-      process.once('SIGINT', () => this.swarm.destroy()) // for faster restarts
+      const topic = Buffer.from(sha256(this.name), 'hex');
+      this.swarm = new Hyperswarm();
+      this.swarm.on('connection', (socket) => this.store.replicate(socket));
+      this.swarm.join(topic);
+      await this.swarm.flush();
+      process.once('SIGINT', () => this.swarm.destroy()); // for faster restarts
     }
 
-    this.info()
+    this.info();
 
-    const self = this
-    const index = this.autobase.createRebasedIndex({
+    const self = this;
+    const view = this.autobase.linearize({
       unwrap: true,
-      async apply (batch) {
-        const b = self.bee.batch({ update: false })
+      async apply(batch) {
+        const b = self.bee.batch({ update: false });
 
         for (const { value } of batch) {
-          const op = JSON.parse(value)
+          const op = JSON.parse(value);
 
           if (op.type === 'post') {
-            const hash = sha256(op.data)
-            await b.put('posts!' + hash, { hash, votes: 0, data: op.data })
+            const hash = sha256(op.data);
+            await b.put('posts!' + hash, { hash, votes: 0, data: op.data });
           }
-
         }
 
-        await b.flush()
-      }
-    })
+        await b.flush();
+      },
+    });
 
-    this.bee = new Hyperbee(index, {
+    this.bee = new Hyperbee(view, {
       extension: false,
       keyEncoding: 'utf-8',
-      valueEncoding: 'json'
-    })
+      valueEncoding: 'json',
+    });
   }
 
-  info () {
-    console.log('Autobase setup. Pass this to run this same setup in another instance:')
-    console.log()
-    console.log('hrepl hypernews.js ' +
-      '-n ' + this.name + ' ' +
-      this.autobase.inputs.map(i => '-w ' + i.key.toString('hex')).join(' ') + ' ' +
-      this.autobase.defaultIndexes.map(i => '-i ' + i.key.toString('hex')).join(' ')
-    )
-    console.log()
-    console.log('To use another storage directory use --storage ./another')
-    console.log('To disable swarming add --no-swarm')
-    console.log()
+  info() {
+    console.log(
+      'Autobase setup. Pass this to run this same setup in another instance:'
+    );
+    console.log();
+    console.log(
+      'hrepl hypernews.js ' +
+        '-n ' +
+        this.name +
+        ' ' +
+        this.autobase.inputs
+          .map((i) => '-w ' + i.key.toString('hex'))
+          .join(' ') +
+        ' ' +
+        this.autobase.defaultOutputs
+          .map((i) => '-i ' + i.key.toString('hex'))
+          .join(' ')
+    );
+    console.log();
+    console.log('To use another storage directory use --storage ./another');
+    console.log('To disable swarming add --no-swarm');
+    console.log();
   }
 
-  async * all () {
-    for await (const data of this.bee.createReadStream({ gt: 'posts!', lt: 'posts!~' })) {
-      yield data.value
+  async *all() {
+    for await (const data of this.bee.createReadStream({
+      gt: 'posts!',
+      lt: 'posts!~',
+    })) {
+      yield data.value;
     }
   }
 
+  async post(text) {
+    const hash = sha256(text);
+
+    await this.autobase.append(
+      JSON.stringify({
+        type: 'post',
+        hash,
+        data: text,
+      })
+    );
+  }
 }
 
-export const hypernews = new Hypernews()
+export const hypernews = new Hypernews();
 
-await hypernews.start()
+await hypernews.start();
 
-function sha256 (inp) {
-  return crypto.createHash('sha256').update(inp).digest('hex')
+function sha256(inp) {
+  return crypto.createHash('sha256').update(inp).digest('hex');
 }
